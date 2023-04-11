d01cae6b4d4b351bd496b81a68ccf8ee5c3052a0
Jeremy Cook <jeremycook123@gmail.com>
2023-04-11 16:04:28 +1200

    updates

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,11 @@ terraform {
       version = "3.66.0"
     }
   }
+  backend "s3" {
+    bucket = "ca-devsecops-terraform-state"
+    key    = "state"
+    region = "us-west-2"
+  }
 }
 
 provider "aws" {
